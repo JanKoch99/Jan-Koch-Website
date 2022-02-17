83d0467d5e42d729b54e444e00d1ecdbfd6536b4
JanKoch99 <jankoch99@bluewin.ch>
2022-02-17 18:02:00 +0100

    updated core

--- Jan-Koch/src/app/app.module.ts
+++ Jan-Koch/src/app/app.module.ts
@@ -2,17 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/components/header/header.component';
-import { FooterComponent } from './core/components/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import {FormsModule} from "@angular/forms";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     HomeComponent,
     ContactComponent
   ],
@@ -20,6 +17,7 @@ import {FormsModule} from "@angular/forms";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
