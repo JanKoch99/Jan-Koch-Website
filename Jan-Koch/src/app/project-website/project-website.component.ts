1a275cfe048fe687e51191150dcbcaa8ebfb0f93
JanKoch99 <jankoch99@bluewin.ch>
2022-02-24 09:24:26 +0100

    added project-website

--- Jan-Koch/src/app/project-website/project-website.component.ts
+++ Jan-Koch/src/app/project-website/project-website.component.ts
@@ -3,9 +3,57 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-project-website',
   template: `
-    <p>
-      project-website works!
-    </p>
+    <section class="section">
+      <div class="container">
+        <div class="columns is-vcentered">
+          <div class="column has-text-centered">
+            <h1 class="is-size-1">
+              Erstellung dieser Website
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <div class="section">
+      <div class="container">
+        
+        <!-- The start -->
+        <div class="columns mt-4">
+          <div class="column is-6">
+            <div class="card">
+              <div class="card-content">
+                <div class="content">
+                  <p class="subtitle">Der Start</p>
+                  <p>
+                    Aller Anfang ist schwer.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Formatierung -->
+            <div class="column is-6">
+              <div class="card">
+                <div class="card-content">
+                  <div class="content">
+                    <p class="subtitle">Die Formatierung</p>
+                    <p>
+                      Jede Formatierung ist schwer.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          
+        </div>
+        
+        
+      </div>
+    </div>   
+    
   `,
   styles: [
   ]
